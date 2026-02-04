e0983150ae43f3cab363155f700698bdf329f373
Daniel Ige <dantomige@gmail.com>
2026-02-04 04:09:14 -0800

    refactor: moved history collection to seperate module

---
+++ services/historyProvider.js
@@ -0,0 +1,23 @@
+class HistoryProvider {
+    static #MAX_HISTORY_RESULTS = 2147483647; // Largest integer for Chrome history API
+
+    /**
+     * 
+     * Access browsing history using Chrome API
+     * 
+     * @param {number} startTime - Timestamp in milliseconds to start fetching from
+     * @param {number} endTime - Timestamp in milliseconds to end fetching at
+     * @param {number} numResults - Maximum number of results to fetch
+     * @returns Promise<chrome.history.HistoryItem[]> - Array of history items
+     */
+    static async fetchHistory(startTime, endTime, numResults = this.#MAX_HISTORY_RESULTS) {
+        return new Promise((resolve) => {
+            chrome.history.search({ 
+                text: '', 
+                maxResults: numResults, 
+                startTime: startTime,
+                endTime: endTime
+            }, resolve);
+        });
+    };
+}
