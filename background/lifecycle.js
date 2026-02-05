12a384a7ff8855f05daf117b2db897a441aed151
Daniel Ige <dantomige@gmail.com>
2026-02-05 05:25:50 -0800

    refactor: rename function

--- background/lifecycle.js
+++ background/lifecycle.js
@@ -1,19 +1,28 @@
 import { handleLoadVocab } from './orchestrator.js';
+import { VocabStorage } from '../storage.js';
 
 
 export function setupLifecycleHandler() {
     chrome.runtime.onInstalled.addListener(async (details) => {
         console.log(`Extension installed/updated. Reason: ${details.reason}`);
-        try {
-            const newSyncTime = Date.now()
-            await bootUpVocabWords(newSyncTime);
-            console.log("Initial vocab boot-up complete.");
-        } catch (error) {
-            console.error("Boot-up failed:", error);
+        
+        if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+            try {
+                const newSyncTime = Date.now()
+                await handleVocabWordsOnInstall(newSyncTime);
+                console.log("Initial vocab boot-up complete.");
+            } catch (error) {
+                console.error("Boot-up failed:", error);
+            };
+        }
+
+        else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+            console.log(`Extension updates. No changes made.`);
         }
     });
 }
 
-async function bootUpVocabWords(newSyncTime) {
+async function handleVocabWordsOnInstall(newSyncTime) {
+    await VocabStorage.clearAllVocabEntries();
     return await handleLoadVocab(newSyncTime);
 };
